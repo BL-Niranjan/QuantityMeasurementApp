3637b4b0e0e99ca0bb0a341730d130ba9e47bd6c
BL-Niranjan <niranjan.manivannan@bridgelabz.com>
2026-06-23 10:29:01 +0530

    [Niranjan] Added : UC2 - Feet and Inches Measurement Equality

--- QuantityMeasurement/src/Test/QuantityMeasurementAppTest.java
+++ QuantityMeasurement/src/Test/QuantityMeasurementAppTest.java
@@ -1,62 +1,122 @@
-package Test;
-
-import Main.QuantityMeasurementApp;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuantityMeasurementAppTest {
 
+    // Feet Tests
+
     @Test
-    void testEquality_SameValue() {
+    void testFeetEquality_SameValue() {
 
-        QuantityMeasurementApp.Feet feet1 =
-                new QuantityMeasurementApp.Feet(1.0);
-
-        QuantityMeasurementApp.Feet feet2 =
-                new QuantityMeasurementApp.Feet(1.0);
-
-        assertTrue(feet1.equals(feet2));
+        assertTrue(
+                QuantityMeasurementApp
+                        .checkFeetEquality(
+                                1.0,
+                                1.0
+                        )
+        );
     }
 
     @Test
-    void testEquality_DifferentValue() {
+    void testFeetEquality_DifferentValue() {
 
-        QuantityMeasurementApp.Feet feet1 =
-                new QuantityMeasurementApp.Feet(1.0);
-
-        QuantityMeasurementApp.Feet feet2 =
-                new QuantityMeasurementApp.Feet(2.0);
-
-        assertFalse(feet1.equals(feet2));
+        assertFalse(
+                QuantityMeasurementApp
+                        .checkFeetEquality(
+                                1.0,
+                                2.0
+                        )
+        );
     }
 
     @Test
-    void testEquality_NullComparison() {
+    void testFeetEquality_SameReference() {
 
         QuantityMeasurementApp.Feet feet =
-                new QuantityMeasurementApp.Feet(1.0);
+                new QuantityMeasurementApp
+                        .Feet(1.0);
+
+        assertTrue(feet.equals(feet));
+    }
+
+    @Test
+    void testFeetEquality_NullComparison() {
+
+        QuantityMeasurementApp.Feet feet =
+                new QuantityMeasurementApp
+                        .Feet(1.0);
 
         assertFalse(feet.equals(null));
     }
 
     @Test
-    void testEquality_NonNumericInput() {
+    void testFeetEquality_NonNumericInput() {
 
         QuantityMeasurementApp.Feet feet =
-                new QuantityMeasurementApp.Feet(1.0);
+                new QuantityMeasurementApp
+                        .Feet(1.0);
 
-        String value = "1.0";
+        assertFalse(
+                feet.equals("1.0")
+        );
+    }
 
-        assertFalse(feet.equals(value));
+    // Inches Tests
+
+    @Test
+    void testInchesEquality_SameValue() {
+
+        assertTrue(
+                QuantityMeasurementApp
+                        .checkInchesEquality(
+                                1.0,
+                                1.0
+                        )
+        );
     }
 
     @Test
-    void testEquality_SameReference() {
+    void testInchesEquality_DifferentValue() {
 
-        QuantityMeasurementApp.Feet feet =
-                new QuantityMeasurementApp.Feet(1.0);
+        assertFalse(
+                QuantityMeasurementApp
+                        .checkInchesEquality(
+                                1.0,
+                                2.0
+                        )
+        );
+    }
 
-        assertTrue(feet.equals(feet));
+    @Test
+    void testInchesEquality_SameReference() {
+
+        QuantityMeasurementApp.Inches inches =
+                new QuantityMeasurementApp
+                        .Inches(1.0);
+
+        assertTrue(inches.equals(inches));
+    }
+
+    @Test
+    void testInchesEquality_NullComparison() {
+
+        QuantityMeasurementApp.Inches inches =
+                new QuantityMeasurementApp
+                        .Inches(1.0);
+
+        assertFalse(inches.equals(null));
+    }
+
+    @Test
+    void testInchesEquality_NonNumericInput() {
+
+        QuantityMeasurementApp.Inches inches =
+                new QuantityMeasurementApp
+                        .Inches(1.0);
+
+        assertFalse(
+                inches.equals("1.0")
+        );
     }
 }
