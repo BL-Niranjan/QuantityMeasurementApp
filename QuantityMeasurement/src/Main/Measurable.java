494a6526a00b0c2b10d62c10a864c60efd0d5bd8
BL-Niranjan <niranjan.manivannan@bridgelabz.com>
2026-06-23 12:56:36 +0530

    [Niranjan] Refactored : UC10 - Generic Quantity Class with Unit Interface for Multi-Category Support

---
+++ QuantityMeasurement/src/Main/Measurable.java
@@ -0,0 +1,12 @@
+package Main;
+
+public interface Measurable
+{
+    double getConversionFactor();
+
+    double convertToBaseUnit(double value);
+
+    double convertFromBaseUnit(double baseValue);
+
+    String getUnitName();
+}
